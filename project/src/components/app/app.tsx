dba6dc4e3a22cc0f6c44eb75b5e5858b8b164695
KrawczowaKris <nukinanada@gmail.com>
2022-09-21 14:08:16 +0500

    Создание главной страницы, компонента для карточки фильма

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+
+type filmInfo = {
+  title: string,
+  genre: string;
+  year: number;
+}
+
+function App(filmInfoProps: filmInfo): JSX.Element {
+  return (
+    <WelcomeScreen filmInfo={ filmInfoProps } />
+  );
 }
 
 export default App;
