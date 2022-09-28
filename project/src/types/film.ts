91a5386b97c5f54c211d63acee062f377cee504d
KrawczowaKris <nukinanada@gmail.com>
2022-09-29 00:17:47 +0500

    4.3 Создание остальных моков для карточек фильмов

--- project/src/types/film.ts
+++ project/src/types/film.ts
@@ -11,7 +11,7 @@ export type TypeFilm = {
   rating: number;
   scoresCount: number;
   director: string;
-  starring: [string];
+  starring: string[];
   runTime: number;
   genre: string;
   released: number;
