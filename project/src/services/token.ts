9b424db954ffd83f3745e95ad6608d7381bdae3f
KrawczowaKris <nukinanada@gmail.com>
2022-11-06 17:58:18 +0500

    8.1.4 Token

--- project/src/services/token.ts
+++ project/src/services/token.ts
@@ -1,4 +1,4 @@
-const AUTH_TOKEN_KEY_NAME = 'guess-film-token';
+const AUTH_TOKEN_KEY_NAME = 'wtw-token';
 
 export type Token = string;
 
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,78 +1,112 @@
-import { createReducer } from '@reduxjs/toolkit';
-import { AuthorizationStatus, CARDS_PER_STEP } from '../const';
+import { createSlice } from '@reduxjs/toolkit';
+import { CARDS_PER_STEP, NameSpace } from '../const';
 import TypeFilm from '../types/film-type';
 import Reviews from '../types/reviews';
-import { changeGenreFilm, increaseCardCount, loadFilms, requireAuthorization, setError, resetMainScreen,
-  resetFilmScreen, changeFilmTab, setDataLoadedStatus } from './action';
 import { SortGenreFilm } from '../utils/functions';
+import { fetchFavoriteFilmsAction, fetchFilmAction, changeFilmStatusToView, fetchOneFilmAction } from '../store/api-actions';
 
 type InitialState = {
   genre: string,
   filmsList: TypeFilm[];
   countShowCard: number,
-  oneGenreList: TypeFilm[],
-  authorizationStatus: string,
-  error: string | null,
+  //oneGenreList: TypeFilm[],
+  // authorizationStatus: string,
+  // error: string | null,
   isDataLoaded: boolean,
-  filmTab: string,
+  // filmTab: string,
   film: TypeFilm | null,
   filmsListFiltered: TypeFilm[],
   reviews: Reviews,
+  // isLoaded: boolean | null,
+  favoriteListFilms: TypeFilm[],
+  favoriteCount: number,
 }
 
 const initialState: InitialState = {
   genre: 'All genres',
   filmsList: [],
   countShowCard: 0,
-  oneGenreList: [],
-  authorizationStatus: AuthorizationStatus.Unknown,
-  error: null,
+  //oneGenreList: [],
+  // authorizationStatus: AuthorizationStatus.Unknown,
+  // error: null,
   isDataLoaded: false,
-  filmTab: 'Overview',
+  // filmTab: 'Overview',
   film: null,
-  filmsListFiltered: [],
+  // filmsListFiltered: [],
   reviews: [],
+  // isLoaded: null,
+  filmsListFiltered: [],
+  favoriteListFilms: [],
+  favoriteCount: 0,
 };
 
 const COUNT_LIST_CARD = 8;
 
-const reducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(changeGenreFilm, (state, action) => {
+export const reducer = createSlice({
+  name: NameSpace.WelcomeScreen,
+  initialState,
+  reducers: {
+    changeGenreFilm: (state, action) => {
       state.genre = action.payload.genre;
-      const oneGenreList = SortGenreFilm(state.filmsList, action.payload.genre);
-      state.countShowCard = oneGenreList.length < CARDS_PER_STEP ? oneGenreList.length : COUNT_LIST_CARD;
-      state.oneGenreList = oneGenreList;
-    })
-    .addCase(increaseCardCount, (state) => {
-      state.countShowCard = (state.countShowCard + CARDS_PER_STEP) < state.oneGenreList.length ?
-        state.countShowCard + CARDS_PER_STEP : state.oneGenreList.length;
-    })
-    .addCase(loadFilms, (state, action) => {
-      state.filmsList = action.payload;
-      state.oneGenreList = action.payload;
-      state.countShowCard = CARDS_PER_STEP;
-    })
-    .addCase(requireAuthorization, (state, action) => {
-      state.authorizationStatus = action.payload;
-    })
-    .addCase(setError, (state, action) => {
-      state.error = action.payload;
-    })
-    .addCase(resetMainScreen, (state) => {
+      const filmsListFiltered = SortGenreFilm(state.filmsList, action.payload.genre);
+      state.countShowCard = filmsListFiltered.length < CARDS_PER_STEP ? filmsListFiltered.length : COUNT_LIST_CARD;
+      state.filmsListFiltered = filmsListFiltered;
+    },
+    increaseCardCount: (state) => {
+      state.countShowCard = (state.countShowCard + CARDS_PER_STEP) < state.filmsListFiltered.length ?
+        state.countShowCard + CARDS_PER_STEP : state.filmsListFiltered.length;
+    },
+    resetCardCount: (state) => {
+      state.countShowCard = state.filmsListFiltered.length < CARDS_PER_STEP ?
+        state.filmsListFiltered.length : COUNT_LIST_CARD;
+    },
+    resetMainScreen: (state) => {
       state.genre = 'All genres';
-      state.oneGenreList = state.filmsList;
+      state.filmsListFiltered = state.filmsList;
       state.countShowCard = state.filmsList.length < CARDS_PER_STEP ? state.filmsList.length : CARDS_PER_STEP;
-    })
-    .addCase(resetFilmScreen, (state) => {
-      state.filmTab = 'Overview';
-    })
-    .addCase(changeFilmTab, (state, action) => {
-      state.filmTab = action.payload.currentTab;
-    })
-    .addCase(setDataLoadedStatus, (state, action) => {
+    },
+    setDataLoadedStatus: (state, action) => {
       state.isDataLoaded = action.payload;
-    });}
-);
+    },
+    setFavoriteCount: (state, action) => {
+      state.favoriteCount = action.payload;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchFilmAction.pending, (state) => {
+        state.isDataLoaded = true;
+      })
+      .addCase(fetchFilmAction.fulfilled, (state, action) => {
+        const films = action.payload;
 
-export default reducer;
+        state.filmsList = films;
+        state.filmsListFiltered = films;
+        state.countShowCard = films.length < CARDS_PER_STEP ? films.length : COUNT_LIST_CARD;
+        state.isDataLoaded = false;
+      })
+      .addCase(fetchOneFilmAction.fulfilled, (state, action) => {
+        state.film = action.payload;
+      })
+      .addCase(fetchFavoriteFilmsAction.pending, (state) => {
+        state.isDataLoaded = true;
+      })
+      .addCase(fetchFavoriteFilmsAction.fulfilled, (state, action) => {
+        state.favoriteListFilms = action.payload;
+        state.favoriteCount = action.payload.length;
+        state.isDataLoaded = false;
+      })
+      .addCase(changeFilmStatusToView.fulfilled, (state, action) => {
+        state.film = action.payload;
+      });
+  }
+});
+
+export const {
+  resetMainScreen,
+  changeGenreFilm,
+  increaseCardCount,
+  resetCardCount,
+  setDataLoadedStatus,
+  setFavoriteCount
+} = reducer.actions;
